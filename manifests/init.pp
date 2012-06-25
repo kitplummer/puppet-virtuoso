9ba8fe1cab6e72cea5b2924882c497b420f83952
Kit Plummer <kitplummer@gmail.com>
2012-06-25 09:44:21 -0700

    supporting source install

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,4 @@
-class virtuoso(
-	$inipath = '/etc/virtuoso-opensource-6.1/', 
-	$dbpath = '/var/lib/virtuoso-opensource-6.1/',
-	$run = true ) {
+class virtuoso(	$run = true ) {
 
 	case $operatingsystem {
 		ubuntu: {
@@ -11,23 +8,38 @@ class virtuoso(
 			package { "virtuoso-vad-isparql": ensure => installed, }
 			package { "virtuoso-vad-ods": ensure => installed, }
 			package { "virtuoso-vad-tutorial": ensure => installed, }
+			$hostingdir = '/usr/lib/virtuoso-opensource-6.1/hosting'
 			$servicename = "virtuoso-opensource-6.1"
+			$dbpath = '/var/lib/virtuoso-opensource-6.1/db/'
+			$sharedir = '/usr/share/virtuoso-opensource-6.1/'
+			$inipath = '/etc/virtuoso-opensource-6.1/'
+			$defaultfile = '/etc/default/virtuoso-opensource-6.1'
+			$version = '-6.1' 
 		}
 		default: {
 			include virtuoso::source
 			$servicename = "virtuoso"
+			$topdir = "/usr/local/virtuoso-opensource/"
+			$hostingdir = "${topdir}/lib/virtuoso/hosting/"
+			$sharedir = "${topdir}/share/virtuoso/"
+			$dbpath = "${topdir}/var/lib/virtuoso/db/"
+			$inipath = "$dbpath"
+			$defaultfile = "/etc/default/virtuoso-opensource"
+			$version = ""
 		}
 	}
 
-	file { "/etc/default/virtuoso-opensource-6.1":
+	file { "$defaultfile":
 		owner => root,
 		group => root,
 		content => template("virtuoso/virtuoso-opensource.erb"),
 		notify => $operatingsystem ? {
 			ubuntu => Service[$servicename],
+			default => Service[$servicename]
 		},
 		require => $operatingsystem ? {
 			ubuntu => Package["virtuoso-opensource"],
+			default => File["/etc/default"]
 		}
 	} 
 
@@ -37,16 +49,19 @@ class virtuoso(
 		content => template("virtuoso/virtuoso.ini.erb"),
 		notify => $operatingsystem ? {
 			ubuntu => Service[$servicename],
+			default => Service[$servicename]
 		},
 		require => $operatingsystem ? {
 			ubuntu => Package["virtuoso-opensource"],
+			default => [Exec["make && make install"], File["/etc/virtuoso-opensource-6.1"]]
 		}
 	}
 
 	service { $servicename:
 		ensure => running,
 		enable => true,
-		hasrestart => true,
-		hasstatus => true,
+		hasrestart => false,
+		hasstatus => false,
+		require => File["/etc/init.d/$servicename"]
 	}
 }
--- manifests/source.pp
+++ manifests/source.pp
@@ -1,24 +1,27 @@
-class virtuoso::source {
+class virtuoso::source ($binpath = "/usr/local/virtuoso-opensource/bin") {
 	### Get, build and install from source code
-	package { "gcc": ensure => installed, },
-	package { "gmake": ensure => installed, }
-	package { "autoconf": ensure => installed, }
-	package { "automake": ensure => installed, }
-	package { "libtool": ensure => installed, }
-	package { "flex": ensure => installed, }
-	package { "bison": ensure => installed, }
-	package { "gperf": ensure => installed, }
-	package { "gawk": ensure => installed, }
-	package { "m4": ensure => installed, }
-	package { "make": ensure => installed, }
-	package { "openssl-devel": ensure => installed, }
-	package { "readline-devel": ensure => installed, }
-	package { "wget": ensure => installed, }
 
-	exec { "wget https://downloads.sourceforge.net/project/virtuoso/virtuoso/6.1.5/virtuoso-opensource-6.1.5.tar.gz?r=&ts=1340319716&use_mirror=voxel": 
+	package { "gcc": ensure => installed, } ->
+	package { "autoconf": ensure => installed, } ->
+	package { "automake": ensure => installed, } ->
+	package { "libtool": ensure => installed, } ->
+	package { "flex": ensure => installed, } ->
+	package { "bison": ensure => installed, } ->
+	package { "gperf": ensure => installed, } ->
+	package { "gawk": ensure => installed, } ->
+	package { "m4": ensure => installed, } ->
+	package { "make": ensure => installed, } ->
+	package { "openssl-devel": ensure => installed, } ->
+	package { "readline-devel": ensure => installed, } ->
+	package { "wget": ensure => installed, } ->
+	package { "python-devel": ensure => installed, } ->
+	package { "unixODBC-devel": ensure => installed, } ->
+
+	exec { "wget https://downloads.sourceforge.net/project/virtuoso/virtuoso/6.1.5/virtuoso-opensource-6.1.5.tar.gz": 
 		cwd => "/tmp",
 		creates => "/tmp/virtuoso-opensource-6.1.5.tar.gz",
-		path => ["/usr/bin", "/bin"]
+		path => ["/usr/bin", "/bin"],
+		timeout => 600, # 4 minutes
 	} ->
 	exec { "tar zxvf virtuoso-opensource-6.1.5.tar.gz": 
 		cwd => "/tmp",
@@ -26,18 +29,34 @@ class virtuoso::source {
 		path => ["/usr/bin/", "/bin"]
 	} ->
 	exec { "./autogen.sh":
+		command => "./autogen.sh",
 		cwd => "/tmp/virtuoso-opensource-6.1.5",
-		creates => "/tmp/virtuoso-opensource-6.1.5",
+		creates => "/tmp/virtuoso-opensource-6.1.5/configure",
 		path => ["/usr/bin/", "/bin"]
 	} ->
-	exec { "./configure":
+	exec { "./configure --with-readline":
+	    command => "/tmp/virtuoso-opensource-6.1.5/configure --with-readline",
 		cwd => "/tmp/virtuoso-opensource-6.1.5",
-		creates => "/tmp/virtuoso-opensource-6.1.5",
-		path => ["/usr/bin/", "/bin"]
+		creates => "/tmp/virtuoso-opensource-6.1.5/Makefile",
+		path => ["/usr/bin/", "/bin", "."]
 	} ->
 	exec { "make && make install":
 		cwd => "/tmp/virtuoso-opensource-6.1.5",
-		creates => "/tmp/virtuoso-opensource-6.1.5",
-		path => ["/usr/bin/", "/bin"]
+		creates => "/usr/local/virtuoso-opensource",
+		path => ["/usr/bin/", "/bin"],
+		timeout => 600, # 10 minutes
 	} ->
+	file { "/etc/default":
+		ensure => directory,
+	} ->
+	file { "/etc/virtuoso-opensource-6.1": 
+		ensure => directory,
+	} ->
+
+	file { "/etc/init.d/virtuoso":
+		owner => root,
+		group => root,
+		mode => 0755,
+		content => template("virtuoso/virtuoso.erb"),
+	}
 }
